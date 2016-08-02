33f716a828301f59be976a5049b48b61be044e23
patgrasso <pgrasso@stevens.edu>
2016-08-01 20:58:33 -0400

    Separate earley from parse() | add valuator to Rule
    The earley function takes several parameters and spits out a set of
    states (one for each token). This should be its own function since it
    must be tested apart from the DFS tree constructor.
    
    The Rule constructor takes an optional third parameter, the valuator,
    which is the function used to evaluate the expression given values for
    each symbol.

---
+++ test.js
@@ -0,0 +1,58 @@
+/*eslint no-console:0*/
+
+
+const parser    = require('./lib/parser');
+//const interp    = require('./interp');
+const readline  = require('readline');
+
+const rl        = readline.createInterface(process.stdin, process.stdout);
+
+
+function printTree(tree, level) {
+  if (tree === undefined) { return; }
+  if (typeof tree === 'object') {
+    console.log(`${Array(level).join(' ')}(${tree.item.lhs.name}`);
+    tree.children.forEach(t => printTree(t, level + 2));
+    console.log(`${Array(level).join(' ')})`);
+    return;
+  }
+  return console.log(`${Array(level).join(' ')}${tree}`);
+}
+
+
+function interpret(parseTree) {
+  if (typeof parseTree === 'string' || parseTree == null) {
+    return parseTree;
+  }
+
+  let values = parseTree.children
+    .map((tree) => interpret(tree))
+    .filter((value) => value != null);
+
+  return parseTree.item.evaluate(values);
+}
+
+
+
+rl.setPrompt('expr> ');
+rl.prompt();
+
+rl.on('line', (line) => {
+  if (line.trim() === '') {
+    return rl.prompt();
+  }
+
+  try {
+    let tree = parser.parse(line.trim());
+    //console.log(JSON.stringify(tree, null, 2));
+    printTree(tree, 1);
+    console.log('=>', interpret(tree));
+  } catch (e) {
+    console.error(e.message);
+  }
+  rl.prompt();
+
+}).on('close', () => {
+  console.log();
+  process.exit(0);
+});
