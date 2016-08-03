c0de0ed9b058a126c6f4a2c412c64078e1d9bf47
patgrasso <pgrasso@stevens.edu>
2016-08-03 18:19:31 -0400

    Document everything, pretty much

--- parsey.js
+++ parsey.js
@@ -1,8 +1,28 @@
+/**
+ * Export module for the parsey package
+ *
+ * @module parsey
+ */
 
 module.exports = {
+  /**
+   * @see module:lib/parser.parse
+   */
   parse     : require('./lib/parser').parse,
+
+  /**
+   * @see module:lib/tokenizer
+   */
   tokenize  : require('./lib/tokenizer'),
+
+  /**
+   * @see module:lib/rules~Rule
+   */
   Rule      : require('./lib/rules').Rule,
+
+  /**
+   * @see module:lib/rules~Sym
+   */
   Sym       : require('./lib/rules').Sym
 };
 
